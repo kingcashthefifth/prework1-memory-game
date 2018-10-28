fa6c711a8fcd2ee98dc23c945ff707a05e1714da
Cash Teo <cash@Cashs-MacBook-Pro.local>
2018-10-28 22:08:12 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,17 @@
-console.log("Up and running!");
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped queen');
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log('User flipped king');
 
-console.log("User flipped " + cardOne);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again.');
+	}
+}
